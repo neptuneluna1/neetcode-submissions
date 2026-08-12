4386f8f252f286fae94e9144532970cb0f618b85
neptuneluna1 <139936424+neptuneluna1@users.noreply.github.com>
2026-08-12 00:28:14 -0400

    Add: range-sum-query-immutable - submission-0

---
+++ Data Structures & Algorithms/range-sum-query-immutable/submission-0.py
@@ -0,0 +1,21 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        if left < 0 or right < 0 or left >= len(self.nums) or right >= len(self.nums):
+            return []
+        sum = 0
+        for i in range(left, right+1):
+            sum += self.nums[i]
+        return sum
+        
+        
+
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
